7361d516a8a4906b8b2711d776c4854692bfd7ef
SamKlep <shamdemann23@gmail.com>
2020-11-21 12:08:06 -0600

    Bird created.

--- src/index.js
+++ src/index.js
@@ -15,19 +15,26 @@ const config = {
   },
 }
 
-// Loading assets, such as images, music, animations etc...
+// Loading assets, such as images, music, animations ...
 function preload() {
   // this context - scene
   // Contains funcs and props we can use
   this.load.image('sky', 'assets/sky.png')
+  this.load.image('bird', 'assets/bird.png')
 }
+
+let bird = null
 
 // Initiliazing application
 function create() {
-  // x
-  // y
+  // x - 400
+  // y - 300
   // key of the image
-  this.add.image(config.width / 2, config.height / 2, 'sky')
+  this.add.image(0, 0, 'sky').setOrigin(0, 0)
+  bird = this.add
+    .sprite(config.width * 0.1, config.height / 2, 'bird')
+    .setOrigin(0)
+  debugger
 }
 
 new Phaser.Game(config)
