3ed7428c48f4179bff682f8c3a8f973569376826
SamKlep <shamdemann23@gmail.com>
2020-11-21 10:42:48 -0600

    Background image inserted.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-
-import Phaser from "phaser";
+import Phaser from 'phaser'
 
 const config = {
+  // WebGL (Web graphics library) JS Api for rendering 2D & 3D graphics
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
+    // Arcade physics plugin, manages physics simulation
     default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
   },
   scene: {
-    preload: preload,
-    create: create
-  }
-};
-
-new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
+    preload,
+    create,
+  },
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+// Loading assets, such as images, music, animations etc...
+function preload() {
+  // this context - scene
+  // Contains funcs and props we can use
+  this.load.image('sky', 'assets/sky.png')
 }
+
+// Initiliazing application
+function create() {
+  // x
+  // y
+  // key of the image
+  this.add.image(config.width / 2, config.height / 2, 'sky')
+}
+
+new Phaser.Game(config)
